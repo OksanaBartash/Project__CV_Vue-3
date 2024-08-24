d7f31d6880f043ee85887766570f0ad29e1b976f
Bartash Oksana <kasandraoks84@gmail.com>
2024-08-24 10:27:44 +0300

    Changed vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/<Project__CV_Vue-3>/' : '/'
-}
+module.exports = defineConfig({
+  transpileDependencies: true
+})
 
-
-//module.exports = defineConfig({
-//  transpileDependencies: true
-//})
+//module.exports = {
+//  publicPath: process.env.NODE_ENV === 'production' ? '/<Project__CV_Vue-3>/' : '/'
+//}
